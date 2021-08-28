e6bebd007eb39aebf045ae06a88a14291456458c
Dwikie Reza Prananda <neithroid@gmail.com>
2021-08-28 15:59:56 +0700

    react 1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { GlobalProvider } from './configs/context';
+import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";
+import { Login } from "./pages/login";
+import { DashboardCustomer } from './pages/dashboard-customer';
+import { DashboardPetugas } from "./pages/dashboard-agent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Router>
+        {/* Link/Anchor */}
+        <Link style={{marginRight: "1.25rem"}} to="/login">Login</Link>
+        <Link style={{marginRight: "1.25rem"}} to="/dashboard-customer">Dashboard Customer</Link>
+        <Link style={{marginRight: "1.25rem"}} to="/dashboard-petugas">Dashboard Petugas</Link>
+        {/* Route Point */}
+        <Route exact path="/">
+            <Redirect to="/login"/>
+        </Route>
+        <Route path="/login" component={Login}/>
+        <Route path="/dashboard-customer" component={DashboardCustomer}/>
+        <Route path="/dashboard-petugas" component={DashboardPetugas}/>
+      </Router>
+    )
 }
 
-export default App;
+export default GlobalProvider(App);
---
+++ src/pages/dashboard-agent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const DashboardPetugas = () => {
+    return (
+        <div>
+            Dashboard Petugas
+        </div>
+    )
+};
