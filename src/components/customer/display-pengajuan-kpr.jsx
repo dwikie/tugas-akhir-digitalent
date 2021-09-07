4f71f22857b947ed8087a40a391b4d7fd5dd79cd
Dwikie Reza Prananda <neithroid@gmail.com>
2021-09-07 14:33:17 +0700

    revert display pengajuan kpr

--- src/components/customer/display-pengajuan-kpr.jsx
+++ src/components/customer/display-pengajuan-kpr.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Table } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 
 export default function DisplayPengajuanKPR(props) {
   const { url } = props.match;
@@ -11,22 +11,67 @@ export default function DisplayPengajuanKPR(props) {
   return (
     <>
       <h3 className="text-center mb-3">Status Pengajuan</h3>
-      <Table striped>
-        <tbody>
+      <Form>
+        <div className="mb-2">
+          <td>
+            {" "}
+            No Induk KTP &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&nbsp;:{" "}
+          </td>
+          <td> &ensp;1234567891000910 </td>
+        </div>
+
+        <div className="mb-2">
+          <td> Nama Lengkap &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&nbsp;: </td>
+          <td> &ensp;Agung Sunandar </td>
+        </div>
+
+        <div className="mb-2">
+          <td> Tempat, Tanggal Lahir &nbsp;: </td>
+          <td> &ensp; Bandung, 7 Juli 1980 </td>
+        </div>
+        <div className="mb-2">
+          <td> Alamat saat ini &ensp;&ensp;&ensp;&ensp;&ensp;&ensp; : </td>
+          <td> &ensp; Jl. Raya Bojongsoang,No.180, Bandung</td>
+        </div>
+        <div className="mb-2">
+          <td>
+            {" "}
+            Pekerjaan
+            &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;:{" "}
+          </td>
+          <td> &ensp; Seniman </td>
+        </div>
+        <div className="mb-2">
+          <td> Pendapatan per bulan : </td>
+          <td> &ensp; Rp 5.000.000</td>
+        </div>
+        <div>
+          <label>
+            {" "}
+            Bukti Selfie KTP &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;:{" "}
+          </label>
+          <Button variant="link">file_selfie_ktp.pdf</Button>
+        </div>
+        <div>
+          <label>
+            {" "}
+            Bukti Slip Gaji &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp; :{" "}
+          </label>
+          <Button variant="link">file_slip_gaji.pdf</Button>
+        </div>
+        <div>
           <tr>
-            <td>Nama</td>
-            <td>xxx</td>
+            <label>
+              {" "}
+              Status Verifikasi &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;: &ensp;{" "}
+            </label>
+            <Button onClick={handleGoToDokumenTambahan}>
+              {" "}
+              Menunggu Verifikasi{" "}
+            </Button>
           </tr>
-          <tr>
-            <td>Nama</td>
-            <td>xxx</td>
-          </tr>
-          <tr>
-            <td>Nama</td>
-            <td>xxx</td>
-          </tr>
-        </tbody>
-      </Table>
+        </div>
+      </Form>
     </>
   );
 }
