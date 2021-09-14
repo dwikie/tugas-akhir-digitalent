c58d8ddb3e20c959bbc330634acf091c614d0a73
diotridia <dio.tridia@gmail.com>
2021-09-14 18:40:11 +0700

    migrateantd:form pengajuan kpr

--- src/components/customer/pengajuan-kpr/form-pengajuan-kpr.jsx
+++ src/components/customer/pengajuan-kpr/form-pengajuan-kpr.jsx
@@ -1,183 +1,167 @@
 import React, { useState } from "react";
-import { Button, Col, Form, InputGroup, Row } from "react-bootstrap";
-import { FaCalendar, FaPaperPlane, FaQuestionCircle } from "react-icons/fa";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import "./form-pengajuan-kpr.css";
-
-export default function FormPengajuanKPR(props) {
+import { Form, Input, Button, DatePicker, InputNumber, Upload } from "antd";
+import { UploadOutlined } from "@ant-design/icons";
+export default function FormPengajuanKPR() {
   const [validated, setValidated] = useState(false);
-  const [selectedDate, setSelectedDate] = useState(null);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setValidated(true);
-    const form = e.currentTarget;
-    if (form.checkValidity() === true) {
-      //go to
+  const [form] = Form.useForm();
+  const normFile = (e: any) => {
+    if (Array.isArray(e)) {
+      return e;
     }
+    return e && e.fileList;
   };
 
+  const handleOnFinish = async (value) => {
+    try {
+      setValidated(true);
+    } catch (err) {
+      setValidated(false);
+      throw new Error(err);
+    }
+  };
   return (
-    <Form noValidate validated={validated} onSubmit={handleSubmit}>
-      <Row className="mb-sm-0 mb-md-2">
-        <Form.Label column md={3} lg={2}>
-          NIK
-        </Form.Label>
-        <Col xs={12} md={9} lg={10}>
-          <Form.Group className="mb-2">
-            <Form.Control type="text" name="nik" required />
-            <Form.Control.Feedback type="invalid">
-              Mohon masukkan <strong>NIK</strong>
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Col>
-      </Row>
-
-      <Row className="mb-sm-0 mb-md-2">
-        <Form.Label column md={3} lg={2}>
-          Nama Lengkap
-        </Form.Label>
-        <Col xs={12} md={9} lg={10}>
-          <Form.Group className="mb-2">
-            <Form.Control type="text" name="nama" required />
-            <Form.Control.Feedback type="invalid">
-              Mohon masukkan <strong>Nama Lengkap</strong>
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Col>
-      </Row>
-
-      <Row className="mb-sm-0 mb-md-2">
-        <Form.Label column md={3} lg={2}>
-          Tempat, Tanggal Lahir
-        </Form.Label>
-        <Col xs={7} md={6} lg={7}>
-          <Form.Group className="mb-2">
-            <Form.Control type="text" name="tempat-lahir" required />
-            <Form.Control.Feedback type="invalid">
-              Mohon masukkan <strong>Tempat Lahir</strong>
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Col>
-        <Col xs={5} md={3} lg={3}>
-          <Form.Group className="mb-2">
-            <DatePicker
-              selected={selectedDate}
-              onChange={(date) => setSelectedDate(date)}
-              className="form-control"
-              name="tanggal-lahir"
-              maxDate={new Date()}
-              autoComplete="off"
-              dateFormat="dd/MM/yyyy"
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              Mohon masukkan <strong>Tanggal Lahir</strong>
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Col>
-      </Row>
-
-      <Row className="mb-sm-0 mb-md-2">
-        <Form.Label column md={3} lg={2}>
-          Alamat Saat Ini
-        </Form.Label>
-        <Col xs={12} md={9} lg={10}>
-          <Form.Group className="mb-2">
-            <Form.Control
-              rows="3"
-              as="textarea"
-              name="alamat"
-              required
-              style={{ maxHeight: "100px" }}
-            />
-            <Form.Control.Feedback type="invalid">
-              Mohon masukkan <strong>Alamat Saat Ini</strong>
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Col>
-      </Row>
-
-      <Row className="mb-sm-0 mb-md-2">
-        <Form.Label column md={3} lg={2}>
-          Pekerjaan
-        </Form.Label>
-        <Col xs={12} md={9} lg={10}>
-          <Form.Group className="mb-2">
-            <Form.Control type="text" name="pekerjaan" required />
-            <Form.Control.Feedback type="invalid">
-              Mohon masukkan <strong>Pekerjaan</strong>
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Col>
-      </Row>
-
-      <Row className="mb-sm-0 mb-md-2">
-        <Form.Label column md={3} lg={2}>
-          Pendapatan Perbulan
-        </Form.Label>
-        <Col xs={12} md={9} lg={10}>
-          <Form.Group className="mb-2">
-            <InputGroup className="mb-3">
-              <InputGroup.Text>Rp.</InputGroup.Text>
-              <Form.Control
-                className="rounded-top rounded-bottom"
-                type="number"
-                name="pendapatan"
-                required
-                inputMode="numeric"
-              />
-              <Form.Control.Feedback type="invalid">
-                Mohon masukkan <strong>Pendapatan Perbulan</strong>
-              </Form.Control.Feedback>
-            </InputGroup>
-          </Form.Group>
-        </Col>
-      </Row>
-
-      <Row className="mb-sm-0 mb-md-2">
-        <Form.Label column md={3} lg={2} className="position-relative">
-          Bukti Selfie KTP
-          <FaQuestionCircle
-            className="position-absolute"
-            style={{
-              width: "10px",
-              height: "10px",
-              right: "10px",
-              color: "#495057",
-            }}
-          />
-        </Form.Label>
-        <Col xs={12} md={9} lg={10}>
-          <Form.Group className="mb-2">
-            <Form.Control type="file" name="selfie-ktp" required />
-            <Form.Control.Feedback type="invalid">
-              Mohon masukkan <strong>Bukti Selfie KTP</strong>
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Col>
-      </Row>
-
-      <Row className="mb-sm-0 mb-md-2">
-        <Form.Label column md={3} lg={2}>
-          Bukti Slip Gaji Suami dan/atau Istri
-        </Form.Label>
-        <Col xs={12} md={9} lg={10}>
-          <Form.Group className="mb-2">
-            <Form.Control type="file" name="slip-gaji" required />
-            <Form.Control.Feedback type="invalid">
-              Mohon masukkan <strong>Bukti Slip Gaji</strong>
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Col>
-      </Row>
-      <div className="d-flex justify-content-end my-3">
-        <Button type="submit" className="d-flex align-items-center">
-          <FaPaperPlane className="me-3" />
-          Submit Pengajuan KPR
-        </Button>
-      </div>
-    </Form>
+    <>
+      <Form
+        id="form-kpr"
+        labelCol={{ span: 4 }}
+        wrapperCol={{ span: 14 }}
+        layout="horizontal"
+        form={form}
+        onFinish={handleOnFinish}
+      >
+        <Form.Item
+          name="nik"
+          label="NIK"
+          rules={[
+            {
+              required: true,
+              message: "Mohon masukkan NIK",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="nama"
+          label="Nama Lengkap"
+          rules={[
+            {
+              required: true,
+              message: "Mohon masukkan Nama Lengkap",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="TL"
+          label="Tempat Lahir"
+          rules={[
+            {
+              required: true,
+              message: "Mohon masukkan Tempat Lahir",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="tanggal"
+          label="Tanggal Lahir"
+          rules={[
+            {
+              required: true,
+              message: "Mohon masukkan Tanggal Lahir",
+            },
+          ]}
+        >
+          <DatePicker />
+        </Form.Item>
+        <Form.Item
+          name="pekerjaan"
+          label="Pekerjaan"
+          rules={[
+            {
+              required: true,
+              message: "Mohon masukkan Pekerjaan",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="pendapatan"
+          label="Pendapatan Perbulan"
+          rules={[
+            {
+              required: true,
+              message: "Mohon masukkan Pendapatan",
+            },
+          ]}
+        >
+          <InputNumber />
+        </Form.Item>
+        <Form.Item
+          name="bukti_selfie"
+          label="Bukti Selfie KTP"
+          rules={[
+            {
+              required: true,
+              message: "Mohon masukkan Bukti Selfie KTP",
+            },
+          ]}
+        >
+          <Form.Item
+            name="selfie"
+            valuePropName="fileList"
+            getValueFromEvent={normFile}
+            noStyle
+          >
+            <Upload.Dragger name="files" action="/upload.do">
+              <p className="ant-upload-drag-icon">
+                <UploadOutlined />
+              </p>
+              <p className="ant-upload-text">
+                Upload file selfie KTP dalam bentuk .pdf
+              </p>
+            </Upload.Dragger>
+          </Form.Item>
+        </Form.Item>
+        <Form.Item
+          name="bukti_slip_gaji"
+          label="Bukti Slip Gaji Suami dan/atau istri"
+          rules={[
+            {
+              required: true,
+              message: "Mohon masukkan Bukti Gaji Suami dan/atau istri",
+            },
+          ]}
+        >
+          <Form.Item
+            name="slip_gaji"
+            valuePropName="fileList"
+            getValueFromEvent={normFile}
+            noStyle
+          >
+            <Upload.Dragger name="files" action="/upload.do">
+              <p className="ant-upload-drag-icon">
+                <UploadOutlined />
+              </p>
+              <p className="ant-upload-text">
+                Upload file slip gaji dalam bentuk .pdf
+              </p>
+            </Upload.Dragger>
+          </Form.Item>
+        </Form.Item>
+      </Form>
+      <Button
+        type="primary"
+        loading={validated}
+        htmlType="submit"
+        style={{ display: "flex", alignItems: "center" }}
+      >
+        <strong>Sumbit Pengajuan KPR</strong>
+      </Button>
+    </>
   );
 }
