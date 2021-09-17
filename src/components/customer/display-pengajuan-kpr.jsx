e341aed0d81eba2c27d1925b0693e557a0c5c18e
Dwikie Reza Prananda <neithroid@gmail.com>
2021-09-17 13:30:00 +0700

    update: env api mock server url

--- src/components/customer/display-pengajuan-kpr.jsx
+++ src/components/customer/display-pengajuan-kpr.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Form, Typography} from "antd";
+import { Button, Form, Typography } from "antd";
 
 export default function DisplayPengajuanKPR(props) {
   const { url } = props.match;
@@ -14,16 +14,19 @@ export default function DisplayPengajuanKPR(props) {
 
   return (
     <>
-     <Typography.Title
+      <Typography.Title
         level={3}
-        style={{ textAlign: "center", margin: "10px 0" }}>
+        style={{ textAlign: "center", margin: "10px 0" }}
+      >
         Status Pengajuan
       </Typography.Title>
 
       <Form>
         <div className="mb-2">
-          <td> No Induk KTP 
-            &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&nbsp;: </td>
+          <td>
+            {" "}
+            No Induk KTP &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&nbsp;:{" "}
+          </td>
           <td> &ensp; 1234567891000910 </td>
         </div>
 
@@ -38,13 +41,14 @@ export default function DisplayPengajuanKPR(props) {
         </div>
 
         <div className="mb-2">
-          <td> Alamat saat ini 
-            &ensp;&ensp;&ensp;&ensp;&ensp;&ensp; : </td>
+          <td> Alamat saat ini &ensp;&ensp;&ensp;&ensp;&ensp;&ensp; : </td>
           <td> &ensp; Jl. Raya Bojongsoang,No.180, Bandung</td>
         </div>
 
         <div className="mb-2">
-          <td> Pekerjaan
+          <td>
+            {" "}
+            Pekerjaan
             &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;:
           </td>
           <td> &ensp; Seniman </td>
@@ -77,32 +81,25 @@ export default function DisplayPengajuanKPR(props) {
               {" "}
               Status Verifikasi &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;: &ensp;{" "}
             </label>
-            <Button type = "primary" onClick={handleGoToDokumenTambahan} >
-            Pengajuan telah terverifikasi, silahkan isi kelengkapan dokumen
+            <Button type="primary" onClick={handleGoToDokumenTambahan}>
+              Pengajuan telah terverifikasi, silahkan isi kelengkapan dokumen
             </Button>
           </tr>
         </div>
-        
 
-      <Typography.Title
-        level={3}
-        style={{ textAlign: "center", margin: "15px 0" }}
-      >
-        Kelengkapan Data KPR
-      </Typography.Title>
-      <div className="mb-2">
-          <td>
-            {" "}
-            Alamat Rumah &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&nbsp;:{" "}
-          </td>
+        <Typography.Title
+          level={3}
+          style={{ textAlign: "center", margin: "15px 0" }}
+        >
+          Kelengkapan Data KPR
+        </Typography.Title>
+        <div className="mb-2">
+          <td> Alamat Rumah &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&nbsp;: </td>
           <td> &ensp;&ensp;Jl. Permai 5 No.11A,Komplek Cipadaung Permai</td>
         </div>
 
-      <div className="mb-2">
-          <td>
-            {" "}
-            Luas Tanah/Rumah &ensp;&ensp;&ensp;:{" "}
-          </td>
+        <div className="mb-2">
+          <td> Luas Tanah/Rumah &ensp;&ensp;&ensp;: </td>
           <td> &ensp;&ensp;500m2</td>
         </div>
 
@@ -115,31 +112,27 @@ export default function DisplayPengajuanKPR(props) {
         </div>
 
         <div className="mb-2">
-          <td>
-            Jangka Pembayaran&ensp;&ensp;&ensp;:&ensp;
-          </td>
+          <td>Jangka Pembayaran&ensp;&ensp;&ensp;:&ensp;</td>
           <td> &ensp;5 Tahun</td>
         </div>
 
         <div>
-          <label>
-            Dokumen Pendukung &ensp;:
-          </label>
+          <label>Dokumen Pendukung &ensp;:</label>
           <Button type="link">file_pendukung.pdf</Button>
         </div>
 
         <div>
           <tr>
-            <label> Status Pengajuan KPR &ensp;: &ensp;{" "} </label>
+            <label> Status Pengajuan KPR &ensp;: &ensp; </label>
             <Button type="primary">Ditolak</Button>
           </tr>
         </div>
 
         <div class="d-flex justify-content-end">
-        <Button type="primary" onClick={handleGoToPengajuanKPR}>
-          Reset Pengajuan KPR 
-        </Button>
-      </div>
+          <Button type="primary" onClick={handleGoToPengajuanKPR}>
+            Reset Pengajuan KPR
+          </Button>
+        </div>
       </Form>
     </>
   );
--- src/components/petugas/detail-pengajuan.jsx
+++ src/components/petugas/detail-pengajuan.jsx
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouteMatch } from "react-router-dom";
+import { Descriptions, Button, Typography } from "antd";
+import { httpAuth } from "../../configs/axios-instances";
 
 export default function DetailPengajuan() {
   const { params } = useRouteMatch();
-  return <div>Detail Pengajuan {params.id}</div>;
+  const { Link } = Typography;
+  const [dataSource, setDataSource] = React.useState({});
+  console.log(params);
+
+  useEffect(() => {
+    const dispDetailPengajuan = async () => {
+      const { data } = await httpAuth
+        .get("/pengajuan", {
+          params: { id_customer: params.id },
+        })
+        .then((res) => res.data)
+        .catch(function (error) {
+          return []; // Return empty array in case error response.
+        });
+      setDataSource(data);
+    };
+    dispDetailPengajuan();
+  }, [params.id]);
+
+  return (
+    <div>
+      <Descriptions title="Data Diri Anda" column={1}>
+        <Descriptions.Item label="Nomor Induk KTP">
+          {dataSource.nik}
+        </Descriptions.Item>
+        <Descriptions.Item label="Nama Lengkap">
+          {dataSource.nama_lengkap}
+        </Descriptions.Item>
+        <Descriptions.Item label="Tempat, Tanggal Lahir">
+          {/* {data2.tempat_lahir}, {data2.tanggal_lahir} */}
+        </Descriptions.Item>
+        <Descriptions.Item label="Alamat saat ini">
+          {/* {data2.alamat} */}
+        </Descriptions.Item>
+        <Descriptions.Item label="Pekerjaan">
+          {/* {data2.pekerjaan} */}
+        </Descriptions.Item>
+        <Descriptions.Item label="Pendapatan per bulan">
+          {/* {data2.pendapatan_perbulan} */}
+        </Descriptions.Item>
+        <Descriptions.Item label="Bukti Selfie KTP">
+          {" "}
+          <Link href="{data2.bukti_ktp}" target="_blank">
+            File_KTP.pdf
+          </Link>
+        </Descriptions.Item>
+        <Descriptions.Item label="Bukti Slip Gaji">
+          <Link href="{data2.bukti_gaji}" target="_blank">
+            File_Slip_Gaji.pdf
+          </Link>
+        </Descriptions.Item>
+        <Descriptions.Item label="Status Verifikasi">
+          <Button type="primary">Terverifikasi</Button>
+        </Descriptions.Item>
+      </Descriptions>
+    </div>
+  );
+  // <div>Detail Pengajuan {params.id}</div>;
 }
