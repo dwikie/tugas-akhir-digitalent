67601509d86dfebf9599002f6e14778374fb858d
BrilyanE41161965 <brilyan.andi@gmail.com>
2021-09-17 15:12:53 +0700

    Detail Pengajuan Petugas

--- src/components/petugas/detail-pengajuan.jsx
+++ src/components/petugas/detail-pengajuan.jsx
@@ -1,36 +1,59 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouteMatch } from "react-router-dom";
-import { Descriptions,Button,Typography  } from 'antd';
-import FormKelengkapanKPR from "./form-kelengkapan-KPR"
-
-
+import { Descriptions, Button, Typography } from "antd";
+import FormKelengkapanKPR from "./form-kelengkapan-KPR";
+import { httpAuth } from "../../configs/axios-instances";
+import apiDetail from "./detail-pengajuan-petugas";
 
 export default function DetailPengajuan() {
-  const { params } = useRouteMatch();
+  // const { params } = useRouteMatch();
   const { Link } = Typography;
-  return (<div>
-    
-   <Descriptions title="Data Diri Anda" column={1} >
-    
-    <Descriptions.Item label="Nomor Induk KTP">Zhou Maomao</Descriptions.Item>
-    <Descriptions.Item label="Nama Lengkap">Nia Kurniasih</Descriptions.Item>
-    <Descriptions.Item label="Tempat, Tanggal Lahir">Bandung, 7 Juli 1980</Descriptions.Item>
-    <Descriptions.Item label="Alamat saat ini">Jl. Raya Bojongsoang, No.180, Bandung</Descriptions.Item>
-    <Descriptions.Item label="Pekerjaan">Seniman</Descriptions.Item>
-    <Descriptions.Item label="Pendapatan per bulan">Rp. 5.000.000</Descriptions.Item>
-    <Descriptions.Item label="Bukti Selfie KTP"> <Link href="" target="_blank">
-      File_KTP.pdf
-    </Link></Descriptions.Item>
-    <Descriptions.Item label="Bukti Slip Gaji"><Link href="" target="_blank">
-      File_Slip_Gaji.pdf
-    </Link></Descriptions.Item>
-    <Descriptions.Item label="Status Verifikasi"><Button type="primary">Terverifikasi</Button></Descriptions.Item>
-   
-  </Descriptions>
-
-  <FormKelengkapanKPR></FormKelengkapanKPR>
   
-  </div>);
+
+//  console.log(dataSource);
+
+
+
+  return (
+    
+    <div>
+      
+      <Descriptions title="Data Diri Anda" column={1}>
+        <Descriptions.Item label="Nomor Induk KTP">
+          
+        </Descriptions.Item>
+        <Descriptions.Item label="Nama Lengkap">
+          
+        </Descriptions.Item>
+        <Descriptions.Item label="Tempat, Tanggal Lahir">
+          
+        </Descriptions.Item>
+        <Descriptions.Item label="Alamat saat ini">
+          
+        </Descriptions.Item>
+        <Descriptions.Item label="Pekerjaan">
+         
+        </Descriptions.Item>
+        <Descriptions.Item label="Pendapatan per bulan">
+          
+        </Descriptions.Item>
+        <Descriptions.Item label="Bukti Selfie KTP">
+          <Link href="" target="_blank">
+            File_KTP.pdf
+          </Link>
+        </Descriptions.Item>
+        <Descriptions.Item label="Bukti Slip Gaji">
+          <Link href="" target="_blank">
+            File_Slip_Gaji.pdf
+          </Link>
+        </Descriptions.Item>
+        <Descriptions.Item label="Status Verifikasi">
+          <Button type="primary">{}</Button>
+        </Descriptions.Item>
+      </Descriptions>
+
+      
+    </div>
+  );
   // <div>Detail Pengajuan {params.id}</div>;
- 
 }
--- src/components/petugas/form-kelengkapan-KPR.jsx
+++ src/components/petugas/form-kelengkapan-KPR.jsx
@@ -1,30 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRouteMatch } from "react-router-dom";
 import { Descriptions, Button, Divider, Row, Select,Typography } from "antd";
 import { LeftOutlined } from "@ant-design/icons";
+import { httpAuth } from "../../configs/axios-instances";
 
 
-export default function FormKelengkapanKPR() {
+
+export default function FormKelengkapanKPR()  {
   const { params } = useRouteMatch();
   
 
   const { Text, Link } = Typography;
   const { Option } = Select;
+ 
+  
 
   function handleChange(value) {
     console.log(`selected ${value}`);
   }
 
+
+
+
+
   return (
+
+
+
     <div>
       <Divider />
       <Descriptions title="Kelengkapan Data KPR" column={1}>
         <Descriptions.Item label="Alamat Rumah">
-          Jl. Permai 5 No. 11 A, Komplek Cipadung Permai
+          {}
         </Descriptions.Item>
         <Descriptions.Item label="Luas Tanah/Rumah">500 m2</Descriptions.Item>
         <Descriptions.Item label="Harga Rumah">
-          Rp. 500.000.000
+          {}
         </Descriptions.Item>
         <Descriptions.Item label="Jangka Pembayaran">5 Tahun</Descriptions.Item>
         <Descriptions.Item label="Dokumen Pendukung">
