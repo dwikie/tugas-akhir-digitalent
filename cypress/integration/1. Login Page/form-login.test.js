3f619675e2480adba415d230170f0b324c4abf88
Dwikie Reza Prananda <neithroid@gmail.com>
2021-09-30 15:59:58 +0700

    test: cypress github actions

--- cypress/integration/1. Login Page/form-login.test.js
+++ cypress/integration/1. Login Page/form-login.test.js
@@ -1,3 +1,5 @@
+/// <reference types="cypress" />
+
 describe("Page Login", () => {
   it("Render Page", () => {
     cy.visit("/login");
@@ -9,16 +11,27 @@ describe("Page Login", () => {
     });
   });
 
-  it("Valid Username & Password", () => {
-    cy.visit("/login");
-    cy.get("form");
-    cy.get("form input[type=text][name=username]").type("valid-username");
-    cy.get("form input[type=password][name=password]").type("valid-password");
-    cy.get("form button[type=submit]").click();
-  });
+  describe("Test Login", () => {
+    it("Valid Username & Password", () => {
+      cy.visit("/login");
+      cy.get("form");
+      cy.get("form input[type=text][name=username]").type("valid-username");
+      cy.get("form input[type=password][name=password]").type("valid-password");
+      cy.get("form button[type=submit]").click();
+      cy.get("form input[type=text][name=username]")
+        .closest(".ant-form-item")
+        .get(".ant-form-item-explain-error")
+        .should("not.exist");
+      cy.get("form input[type=password][name=password]")
+        .closest(".ant-form-item")
+        .get(".ant-form-item-explain-error")
+        .should("not.exist");
+      cy.location().should((loc) => {
+        expect(loc.pathname).to.equal("/dashboard");
+      });
+    });
 
-  describe("Invalid Username/Password", () => {
-    it("Empty Username & Password has Error Feedback", () => {
+    it("Empty Username & Password", () => {
       cy.visit("/login");
       cy.get("form");
       cy.get("form input[type=text][name=username]");
@@ -27,11 +40,15 @@ describe("Page Login", () => {
       cy.get("form input[type=text][name=username]")
         .closest(".ant-form-item")
         .get(".ant-form-item-explain-error")
-        .contains("exist");
+        .should("exist");
       cy.get("form input[type=password][name=password]")
         .closest(".ant-form-item")
         .get(".ant-form-item-explain-error")
         .should("exist");
+      cy.location().should((loc) => {
+        expect(loc.pathname).to.not.equal("/dashboard");
+        expect(loc.pathname).to.equal("/login");
+      });
     });
   });
 });
