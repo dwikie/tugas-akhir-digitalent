2bf4b563f9df934e4e8f02c2de66d2d8726812d9
Dwikie Reza Prananda <neithroid@gmail.com>
2021-10-11 17:32:03 +0700

    update/cd-app-engine

---
+++ app.yaml.sh
@@ -0,0 +1,17 @@
+env: standard
+runtime: nodejs10
+instance_class: F1
+automatic_scaling:
+  max_instances: 1
+  min_instances: 0
+  target_cpu_utilization: 0.9
+service: $TEAM_NAME
+handlers:
+  - url: /static
+    static_dir: build/static
+  - url: /(.*\.(json|ico|js))$
+    static_files: build/\1
+    upload: build/.*\.(json|ico|js)$
+  - url: .*
+    static_files: build/index.html
+    upload: build/index.html
